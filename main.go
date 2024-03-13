9d12ca7efe9bbb7754078973f505623f3a92ea9b
kaarup <vares.kaarup@gmail.com>
2024-03-13 23:05:08 +0200

    basic template

--- main.go
+++ main.go
@@ -9,21 +9,15 @@ const PORT = ":8080"
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is the home page")
+
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := AddValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2 is %d", sum))
-}
 
-func AddValues(x, y int) int {
-	return x + y
 }
 
 func main() {
-
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
