c11ba3aec389ec1daf3d3355135d4f2c5208f31e
kaarup <vares.kaarup@gmail.com>
2024-03-13 22:15:10 +0200

    starting point

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"net/http"
+)
+
+const PORT = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2 is %d", sum))
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
 
 func main() {
-	myMap := make(map[string]string)
-	myMap["dog"] = "samson"
 
-	log.Println(myMap["dog"])
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting port on %s", PORT))
+	_ = http.ListenAndServe(PORT, nil)
 }
